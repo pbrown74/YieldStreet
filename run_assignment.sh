acc4a78a44cb6855f0befcc233a6b928cf22b503
pbrown74 <pbrown74home@gmail.com>
2024-11-07 23:43:13 +0100

    initial commit

--- run_assignment.sh
+++ run_assignment.sh
@@ -7,6 +7,6 @@ git clone https://github.com/pbrown74/YieldStreet
 docker build -t assignment-0.0.1 .
 
 # this will pull MySQL and RabbitMQ images + JDK 17 , then it will try start the assignment
-# but for me the first time it tried it pulled everything but failewd to start the assignment.
+# but for me the first time i tried, it pulled everything but failed to start the assignment.
 # i ran the same command a second time and it worked!!
 docker compose up
